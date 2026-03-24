4f3fa562d2ea7e345986ea400042fb1db5398db8
Atherva Srivastava <srivastavaatherva@gmail.com>
2026-03-24 23:24:00 +0530

    Main.java for BankingApp

--- Experiment 5/BankingApp.java
+++ Experiment 5/BankingApp.java
@@ -1,29 +1,26 @@
 import java.util.ArrayList;
-import java.util.List;
-// Base Class
-abstract class Account {
-    protected String accountNumber;
-    protected double balance;
 
-    public Account(String accountNumber, double balance) {
-        this.accountNumber = accountNumber;
-        this.balance = balance;
-    }
+public class BankingApp {
+    public static void main(String[] args) {
+        // List of Customers
+        ArrayList<Customer> customers = new ArrayList<>();
 
-    public void deposit(double amount) {
-        if (amount > 0) {
-            balance += amount;
-            System.out.println("Deposited ₹" + amount + " to " + accountNumber);
+        // Setup Customer 1
+        Customer c1 = new Customer("Atherva Sri");
+        c1.addAccount(new SavingsAccount("SAV-101", 5000.0));
+        c1.addAccount(new LoanAccount("LOAN-202", 15000.0, 50000.0));
+        
+        // Setup Customer 2
+        Customer c2 = new Customer("Aadya Rastogi");
+        c2.addAccount(new SavingsAccount("SAV-303", 1200.0));
+
+        customers.add(c1);
+        customers.add(c2);
+
+        // Display Consolidated Information
+        System.out.println(" GLOBAL BANKING REPORT ");
+        for (Customer c : customers) {
+            c.displayProfile();
         }
     }
-
-    // Abstract to force specific logic in child classes
-    public abstract void withdraw(double amount);
-
-    public String getDetails() {
-        return "Account: " + accountNumber + " | Balance: ₹" + balance;
-    }
 }
-
-
-
